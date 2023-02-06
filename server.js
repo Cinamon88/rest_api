301df821f04967d6eaf4423f0d7b92e8dd1c649c
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-06 18:35:39 +0100

    fix endpoints

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const uuid = require('uuid').v4;
 
@@ -11,21 +12,22 @@ app.use(cors());
 const db = [
     { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
     { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
+  ];
 
-app.get('/testimonials', (res, req) => {
-    res.json(db);
+
+app.get('/testimonials', (req, res) => {
+    res.json(db);  
 });
 
-app.get('/testimonials/:id', (res, req) => {
+app.get('/testimonials/:id', (req, res) => {
     res.json(db.find((data) => data.id == req.params.id));
 });
 
-app.get('/testimonials/random', (res, req) => {
-    res.json(db);
+app.get('/testimonials/random', (req, res) => {
+    res.json(db);  
 });
 
-app.post('/testimonials', (res, req) => {
+app.post('/testimontials', (req, res) => {
     const { author, text } = req.body;
     const id = uuid();
     const newTestimonial = { id: id, author: author, text: text };
@@ -33,19 +35,19 @@ app.post('/testimonials', (res, req) => {
     res.json({ message: 'ok!' });
 });
 
-app.put('/testimonials/:id', (res, req) => {
+app.put('/testimontials/:id',(req, res) => {
         const { author, text } = req.body;
-        const id = +req.params.id
+        const id = +req.params.id;
         const testimonial = db.find((testimonial) => testimonial.id === id);
         testimonial.author = author;
         testimonial.text = text;
-        res.json({ message: 'ok!' }); },
+        res.json({ message: 'ok!' });    },
     (err) => {
         console.log(err);
     }
 );
 
-app.delete('/testimonials/:id', (res, req) => {
+app.delete('/testimontials/:id',(req, res) => {
         const id = +req.params.id;
         db.splice(
             db.findIndex((testimonial) => testimonial.id === id),
@@ -58,10 +60,11 @@ app.delete('/testimonials/:id', (res, req) => {
     }
 );
 
+
 app.use((req, res) => {
-    res.json(404).send('Not found...');
+    res.status(404).send('Not found...');
 });
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+    console.log('Server is running on port: 8000');
 });
