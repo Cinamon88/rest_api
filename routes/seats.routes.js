a51a9696a5b055ebcdc8f86048939ac0e92f652a
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-08 08:27:59 +0100

    add func some and update endpoints

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -8,38 +8,53 @@ router.route('/seats').get((req, res) => {
     res.json(db.seats);  
 });
 
+router.route('/seats/random').get((req, res) => {
+    res.json(db.seats[Math.floor(Math.random() * db.seats.length)]);  
+});
+
 router.route('/seats/:id').get((req, res) => {
-    res.json(db.seats.find((data) => data.id == req.params.id));
+    res.json(db.seats.find((seat) => seat.id == req.params.id));
 });
 
 router.route('/seats').post((req, res) => {
-    const { author, text } = req.body;
+    const { day, seat, client, email } = req.body;
     const id = uuid();
-    const newSeats = { id: id, author: author, text: text };
-    db.seats.push(newSeats);
-    res.json({ message: 'ok!' });
+    const newSeats = { id: id, day, seat, client, email };
+    if(db.seats.some((checkSeat) => checkSeat.day == newSeats.day && checkSeat.seat == newSeats.seat)) {
+        res.json({ message: 'The slot is already taken' });
+        res.status(409).json({ message: 'The slot is already taken!'});
+    } else {
+        db.seats.push(newSeats);
+        res.json({ message: 'ok!' });
+    }
 });
 
 router.route('/seats/:id').put((req, res) => {
-    const { author, text } = req.body;
+    const { day, seat, client, email } = req.body;
     const id = +req.params.id;
-    const seats = db.seats.find((data) => data.id === id);
-    const index = db.seats.indexOf(seats);
-    const changeSeats = {
-        id: id,
-        author: author,
-        text: text,
-    };
-    db.seats[index] = changeSeats
-    res.json({ message: 'ok!' });    },
+    const seats = db.seats.find((seat) => seat.id === id);
+    seats.day = day;
+    seats.seat = seat;
+    seats.client = client;
+    seats.email = email;
+    res.json({ message: 'ok!' });    
+    },
+    (err) => {
+        console.log(err);
+    }
 );
 
 router.route('/seats/:id').delete((req, res) => {
-    const element = db.seats.find((data) => data.id == req.params.id);
-    const index = db.seats.indexOf(element);
-
-    db.seats.splice(index, 1);
-    res.json({ message: 'ok' });
-});
+        const id = +req.params.id;
+        db.seats.splice(
+            db.seats.findIndex((seat) => seat.id === id),
+            1 
+        );
+        res.json({ message: 'deleted' });
+    },
+    (err) => {
+        console.log(err);
+    }
+);
 
 module.exports = router;
