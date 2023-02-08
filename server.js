19d2c0556206ca64beab854381e17ae1a034bd81
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-08 10:40:42 +0100

    add README

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api/', testimonials);
