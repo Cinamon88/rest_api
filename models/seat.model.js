e7cc3932ba00f8df4d7622de6e6d3ee2e0c77c02
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-17 20:32:07 +0100

    add models

---
+++ models/seat.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const seatSchema = new mongoose.Schema({
+    id: { type: String, required: true },
+    day: { type: String, required: true },
+    seat: { type: String, required: true },
+    client: { type: String, required: true },
+    email: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Seat', seatSchema);
