36ebd0fd6e005e54f8e575e03b9e9977016eccb5
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-17 20:19:38 +0100

    add mongodb & mongoose and connect to db

--- server.js
+++ server.js
@@ -1,44 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-const socket = require('socket.io');
-
-const app = express();
+const mongoose = require('mongoose');
 
 const testimonials = require('./routes/testimonials.routes');
 const concerts = require('./routes/concerts.routes');
 const seats = require('./routes/seats.routes');
 
-app.use((req, res, next) => {
-    req.io = io;
-    next();
-});
+const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/client/build')));
-
-app.use('/api/', testimonials);
-app.use('/api/', concerts);
-app.use('/api/', seats);
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
+app.use('/api', testimonials);
+app.use('/api', concerts);
+app.use('/api', seats);
 
 app.use((req, res) => {
-    res.status(404).send('Not found...');
+  res.status(404).send({ message: 'Not found...' });
+})
+
+// connects our backend code with the database
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
 });
+db.on('error', err => console.log('Error ' + err));
 
-const server = app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is running on port: 8000');
-});
-
-const io = socket(server);
-
-io.on('connection', (socket) => {
-    console.log('New Socket!', + socket.id)
+app.listen('8000', () => {
+  console.log('Server is running on port: 8000');
 });
